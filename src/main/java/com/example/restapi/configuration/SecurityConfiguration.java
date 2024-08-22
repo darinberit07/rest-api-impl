62f34eb4ff9decb5e2e98ba224f7e8c9029fa63e
Darin Berit R S <darinberitrs@gmail.com>
2024-08-22 13:07:26 +0530

    Added Security configuration

---
+++ src/main/java/com/example/restapi/configuration/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.example.restapi.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.restapi.service.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	
+		http
+			.csrf(customizer -> customizer
+				.disable()
+			)
+			.authorizeHttpRequests(request -> request
+				.anyRequest()
+				.authenticated()
+			)
+			.formLogin(Customizer.withDefaults())
+			.httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+	}
+    
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+    	DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+    	daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+    	daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+    	return daoAuthenticationProvider;
+    }
+    
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+    	return new BCryptPasswordEncoder(12);
+    }
+}
