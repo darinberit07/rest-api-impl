d9f9b00acafe8b46a0236badf48ca5e223c73579
Darin Berit R S <darinberitrs@gmail.com>
2025-04-14 17:46:04 +0530

    Add repository annotation

--- src/main/java/com/example/restapi/repository/LoginCredentialsRepository.java
+++ src/main/java/com/example/restapi/repository/LoginCredentialsRepository.java
@@ -1,9 +1,11 @@
 package com.example.restapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.restapi.model.LoginCredentialsModel;
 
+@Repository
 public interface LoginCredentialsRepository extends JpaRepository<LoginCredentialsModel, Long>{
 	LoginCredentialsModel findByUsername(String username);
 }
